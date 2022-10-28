a61577357e44d9d90499934a0b8b388274b18323
chiluksr <chiluksr@oregonstate.edu>
2022-10-27 22:48:58 -0700

    commit

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -7,7 +7,7 @@ final class codeTests: XCTestCase {
         let Table = try CrackStation.loadDictionaryFromDisk(key: "84a516841ba77a5b4648de2cd0dfcb30ea46dbb4")
         // check if the Table is nil or not
         if Table == "nil"{
-            print("nothing, just nil")
+            print("nil")
         }
         else{
             print(Table)
