2163a076beceb5254dcdbfa12d54e54e8af0f3e9
chiluksr <chiluksr@oregonstate.edu>
2022-12-01 22:55:30 -0800

    updated the file

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -4,28 +4,28 @@ import Foundation
 // Implementing the Decrypter protocol to swift
 public class CrackStation: Decrypter{
     // declaring a swift library to load the json data into it
-    private var dict = Dictionary<String,String>()
+    private let hashes_to_plaintextDictionary: [String:String]
 
     required public init(){
         do{
-            dict = try loadDictionaryFromDisk()
+            hashes_to_plaintextDictionary = try CrackStation.loadDictionaryFromDisk()
         }
         catch{
-            dict = ["":""]
+            hashes_to_plaintextDictionary = [:]
         }
     }
 
     public func decrypt(shaHash:String) -> String?{
-        let value = dict[shaHash]
-        return value       
+        let plain_text_password = hashes_to_plaintextDictionary[shaHash]
+        return plain_text_password       
     }
 
-    private func loadDictionaryFromDisk() throws -> Dictionary<String,String>{
-        guard let filepath = Bundle.module.url(forResource: "data", withExtension: "json") else {return ["":""]} 
-        guard let json_data = try? Data(contentsOf: filepath) else {return ["":""]}
+    static func loadDictionaryFromDisk() throws -> Dictionary<String,String>{
+        guard let json_file_path = Bundle.module.url(forResource: "data", withExtension: "json") else {return ["":""]} 
+        guard let json_data = try? Data(contentsOf: json_file_path) else {return ["":""]}
         let jsonResult = try? JSONSerialization.jsonObject(with: json_data)
-        guard let table = jsonResult as? Dictionary<String, String> else { return ["":""]}
-        return table
+        guard let hash_table = jsonResult as? Dictionary<String, String> else { return ["":""]}
+        return hash_table
     }
 
 }
